376787e6410fddc2cca135e5fd9bed1cd8da59e6
LouisFitd <65094430+LouisFitdevoie@users.noreply.github.com>
2022-08-11 15:58:41 +0200

    Request to get all users

--- src/api/controllers/user.controller.js
+++ src/api/controllers/user.controller.js
@@ -10,10 +10,21 @@ class User {
     this.lastName = lastName;
     this.password = password;
   }
-  id = uuid.va4();
+  id = uuid.v4();
   emailAddress;
   firstName;
   lastName;
   password;
   deleted_at = null;
+}
+
+exports.getAllUsers = (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    connection.query('SELECT * FROM users WHERE deleted_at IS null', (err, result) => {
+      connection.release();
+      if (err) throw err;
+      res.send(result);
+    });
+  });
 }
