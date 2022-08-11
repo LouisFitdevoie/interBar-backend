20d28320316530075d83ddfae47650a7e156fcef
LouisFitd <65094430+LouisFitdevoie@users.noreply.github.com>
2022-08-11 17:08:37 +0200

    Request to get users with firstname and lastname

--- src/api/controllers/user.controller.js
+++ src/api/controllers/user.controller.js
@@ -50,3 +50,23 @@ exports.getUserWithId = (req, res) => {
     res.status(400).send({ 'error': 'Invalid user id, ' + req.query.id + ' is not a valid uuid' });
   }
 }
+exports.getUserWithName = (req, res) => {
+  if (req.query.firstName.trim().length > 0 && req.query.lastName.trim().length > 0) {
+    pool.getConnection((err, connection) => {
+      connection.query('SELECT * FROM users WHERE firstname LIKE ? AND lastname LIKE ? AND deleted_at IS null', ['%' + req.query.firstName.trim() + '%', '%' + req.query.lastName.trim() + '%'], (err, result) => {
+        connection.release();
+        if (err) throw err;
+        if (result.length > 0) {
+          console.log('Number of users found: ' + result.length + '');
+          res.send(result);
+        } else {
+          console.log('No users found');
+          res.status(404).send({ 'error': 'No users found for the name ' + req.query.firstName + ' ' + req.query.lastName });
+        }
+      });
+    });
+  } else {
+    console.log('Missing firstname or lastname');
+    res.status(400).send({ 'error': 'Missing firstname or lastname' });
+  }
+}
