19f55e93357a518a2571f59c15e4bda8a90b9b98
LouisFitd <65094430+LouisFitdevoie@users.noreply.github.com>
2022-08-11 18:24:04 +0200

    User can edit his password

--- src/api/controllers/user.controller.js
+++ src/api/controllers/user.controller.js
@@ -93,7 +93,7 @@ exports.getUserWithEmail = (req, res) => {
   }
 }
 
-//ADD VERIFICATION OF CONFIRM PASSWORD
+//ADD VERIFICATION OF CONFIRM PASSWORD AND VERIFY user does not already exist even if deleted
 
 exports.createUser = (req, res) => {
   if (req.body.emailAddress.trim().length > 0 && emailRegex.test(req.body.emailAddress.trim())) {
@@ -127,7 +127,45 @@ exports.updateUser = (req, res) => {
 
 }
 exports.updateUserPassword = (req, res) => {
-
+  if (req.body.newPassword.trim().length > 7 && req.body.newPassword.trim().match(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/)) {
+    if (req.body.newPasswordConfirmation.trim() === req.body.newPassword.trim()) {
+      if (req.body.oldPassword.trim().length > 0) {
+        pool.getConnection((err, connection) => {
+          if (err) throw err;
+          let passwordHashed = bcrypt.hashSync(req.body.newPassword.trim(), 10);
+          connection.query('SELECT password FROM users WHERE id = ? AND deleted_at IS null', [req.body.id], (err, result) => {
+            if (err) throw err;
+            if (result.length > 0) {
+              if (bcrypt.compareSync(req.body.oldPassword.trim(), result[0].password) && !bcrypt.compareSync(req.body.newPassword.trim(), result[0].password)) {
+                connection.query('UPDATE users SET password = ? WHERE id = ? AND deleted_at IS null', [passwordHashed, req.body.id], (err, result) => {
+                  connection.release();
+                  if (err) throw err;
+                  console.log('Password updated');
+                  res.send({ 'message': 'Password updated' });
+                });
+              } else {
+                connection.release();
+                console.log('Invalid old password');
+                res.status(400).send({ 'error': 'Invalid old password' });
+              }
+            } else {
+              console.log('No users found');
+              res.status(404).send({ 'error': 'No users found for the id ' + req.body.id });
+            }
+          });
+        });
+      } else {
+        console.log('Missing old password');
+        res.status(400).send({ 'error': 'Missing old password' });
+      }
+    } else {
+      console.log('New password confirmation does not match new password');
+      res.status(400).send({ 'error': 'New password confirmation does not match new password' });
+    }
+  } else {
+    console.log('Invalid password');
+    res.status(400).send({ 'error': 'Invalid password. Password must be at least 8 characters and contains at least one letter, at least one number and at least one special character' });
+  }
 }
 exports.login = (req, res) => {
   if (req.body.emailAddress.trim().length > 0 && emailRegex.test(req.body.emailAddress.trim())) {
