ee1368a64752e4338735052d08a397352865a1f7
LouisFitd <65094430+LouisFitdevoie@users.noreply.github.com>
2022-11-18 12:25:19 +0100

    Forgot to include the client_name when getting all the commands of an event

--- src/api/controllers/command.controller.js
+++ src/api/controllers/command.controller.js
@@ -133,7 +133,7 @@ exports.getCommandsByEventId = (req, res) => {
       if (err) throw err;
       console.log(`Getting commands with event id ${req.params.id}`);
       connection.query(
-        "SELECT commands.id, commands.client_id, commands.servedBy_id, commands.event_id, commands.isServed, commands.isPaid, commands.created_at, commands.deleted_at, events_products_commands.id AS events_products_commands_id, events_products_commands.command_id, events_products_commands.event_product_id, events_products_commands.number, events_products_commands.deleted_at AS events_products_commands_deleted_at FROM commands LEFT JOIN events_products_commands ON commands.id = events_products_commands.command_id WHERE commands.event_id = ? AND commands.deleted_at IS null",
+        "SELECT commands.id, commands.client_id, commands.client_name, commands.servedBy_id, commands.event_id, commands.isServed, commands.isPaid, commands.created_at, commands.deleted_at, events_products_commands.id AS events_products_commands_id, events_products_commands.command_id, events_products_commands.event_product_id, events_products_commands.number, events_products_commands.deleted_at AS events_products_commands_deleted_at FROM commands LEFT JOIN events_products_commands ON commands.id = events_products_commands.command_id WHERE commands.event_id = ? AND commands.deleted_at IS null",
         [req.params.id],
         (err, result) => {
           connection.release();
@@ -148,6 +148,7 @@ exports.getCommandsByEventId = (req, res) => {
                 let newCommand = {
                   id: command.id,
                   client_id: command.client_id,
+                  client_name: command.client_name,
                   servedBy_id: command.servedBy_id,
                   event_id: command.event_id,
                   isServed: command.isServed,
