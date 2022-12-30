73b7c232b5a0c57ed14c041f6fb6c97c6a4213dd
LouisFitd <65094430+LouisFitdevoie@users.noreply.github.com>
2022-12-30 15:07:30 +0100

    Testing POST /create-event-product-command

--- src/test/api/event.test.js
+++ src/test/api/event.test.js
@@ -15,7 +15,7 @@ const startDate = new Date(
   new Date().setDate(new Date().getDate() + 1)
 ).toISOString();
 const startNowDate = new Date(
-  new Date().setSeconds(new Date().getSeconds() + 15) //IF ERROR DURING THE TEST FOR CREATING AN EVENT WITH START DATE NOW, EDIT THE NUMBER OF SECONDS TO ADD
+  new Date().setSeconds(new Date().getSeconds() + 17) //IF ERROR DURING THE TEST FOR CREATING AN EVENT WITH START DATE NOW, EDIT THE NUMBER OF SECONDS TO ADD
 ).toISOString();
 const endDate = new Date(
   new Date().setDate(new Date().getDate() + 2)
